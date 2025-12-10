c7ec5332b5d3e2e92e03d86c1498911dcb77cddd
Patrick <binary_thinker@yahoo.com>
2025-12-09 17:59:29 -0800

    Latest

--- app/src/main/java/edu/sfsu/thecocktaildb/ui/home/HomeFragment.java
+++ app/src/main/java/edu/sfsu/thecocktaildb/ui/home/HomeFragment.java
@@ -1,35 +1,211 @@
 package edu.sfsu.thecocktaildb.ui.home;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import edu.sfsu.thecocktaildb.Model.CocktailModel;
+import edu.sfsu.thecocktaildb.R;
 import edu.sfsu.thecocktaildb.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+    private MutableLiveData<ArrayList<CocktailModel>> liveData;
+    public ArrayList<CocktailModel> model = new ArrayList<>();
     private FragmentHomeBinding binding;
-
+    private static final String URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+    /**
+     * Note: onCreateView acts like a constructor.
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return
+     */
+    HomeViewModel homeViewModel;
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        // @SuppressLint("SimpleDateFormat") SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
 
+        // i thought this was initialized in the viewmodel, i was wrong :^(
+        liveData = new MutableLiveData<>(); // this is already declared when HomeViewModel is called
+
+        new CocktailAsyncTask().execute(URL);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
-        final TextView textView = binding.textHome;
-        // homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        // stopped here: trying to get data to show from the model here
+        // 12.9.25, 11:54:32
+        homeViewModel.getLiveData().observe(getViewLifecycleOwner(), data-> {
+            Log.i("log", "data.get(9).getStrDrink() -> " + data.get(9).getStrDrink());
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NotNull Context context) {
+        super.onAttach(context);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    // Inner Class - there is NOT a static keyword!
+    public class CocktailAsyncTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.v("log", "onPreExecute()");
+        }
+
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  doInBackground
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection httpURLConnection = null;
+            String line = null;
+            String results = null;
+
+            Log.i("log", params[0]);
+
+            try {
+                URL url = new URL(params[0]);
+
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                int code = httpURLConnection.getResponseCode();
+
+                if(code != 200)
+                    throw new IOException("Invalid response from the server: " + code);
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+
+                // FileOutputStream out = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+                //byte[] buffer = new byte[1024];
+                // int bytesRead = bufferedReader.read(buffer);
+
+                StringBuilder builder = new StringBuilder();
+
+                while((line = bufferedReader.readLine()) != null) {
+                    builder.append(line).append("\n");
+                    // out.write(1221);
+                    Log.i("log", "line => " + line);
+                }
+
+                if(builder.length() == 0) {
+                    return null;
+                }
+
+                results = builder.toString();
+
+                return results;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if(httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+            }
+
+            return null;
+        }
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  onPostExecute
+        // onPostExecute - executes on the 'Main Thread'
+        @Override
+        protected void onPostExecute(String result) { // onPostExecute - runs on the main thread.
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray obj = jsonObject.getJSONArray("drinks");
+
+                for(int i =  0; i < obj.length(); i++) {
+                    model.add(new CocktailModel(
+                            obj.getJSONObject(i).getString("idDrink"),
+                            obj.getJSONObject(i).getString("strDrink"),
+                            obj.getJSONObject(i).getString("strDrinkAlternate"),
+                            obj.getJSONObject(i).getString("strTags"),
+                            obj.getJSONObject(i).getString("strVideo"),
+                            obj.getJSONObject(i).getString("strCategory"),
+                            obj.getJSONObject(i).getString("strIBA"),
+                            obj.getJSONObject(i).getString("strAlcoholic"),
+                            obj.getJSONObject(i).getString("strGlass"),
+                            obj.getJSONObject(i).getString("strInstructions"),
+                            obj.getJSONObject(i).getString("strInstructionsES"),
+                            obj.getJSONObject(i).getString("strInstructionsDE"),
+                            obj.getJSONObject(i).getString("strInstructionsFR"),
+                            obj.getJSONObject(i).getString("strInstructionsIT"),
+                            obj.getJSONObject(i).getString("strInstructionsZH-HANS"),
+                            obj.getJSONObject(i).getString("strInstructionsZH-HANT"),
+                            obj.getJSONObject(i).getString("strDrinkThumb"),
+                            obj.getJSONObject(i).getString("strIngredient1"),
+                            obj.getJSONObject(i).getString("strIngredient2"),
+                            obj.getJSONObject(i).getString("strIngredient3"),
+                            obj.getJSONObject(i).getString("strIngredient4"),
+                            obj.getJSONObject(i).getString("strIngredient5"),
+                            obj.getJSONObject(i).getString("strIngredient6"),
+                            obj.getJSONObject(i).getString("strIngredient7"),
+                            obj.getJSONObject(i).getString("strIngredient8"),
+                            obj.getJSONObject(i).getString("strIngredient9"),
+                            obj.getJSONObject(i).getString("strIngredient10"),
+                            obj.getJSONObject(i).getString("strIngredient11"),
+                            obj.getJSONObject(i).getString("strIngredient12"),
+                            obj.getJSONObject(i).getString("strIngredient13"),
+                            obj.getJSONObject(i).getString("strIngredient14"),
+                            obj.getJSONObject(i).getString("strIngredient15"),
+                            obj.getJSONObject(i).getString("strMeasure1"),
+                            obj.getJSONObject(i).getString("strMeasure2"),
+                            obj.getJSONObject(i).getString("strMeasure3"),
+                            obj.getJSONObject(i).getString("strMeasure4"),
+                            obj.getJSONObject(i).getString("strMeasure5"),
+                            obj.getJSONObject(i).getString("strMeasure6"),
+                            obj.getJSONObject(i).getString("strMeasure7"),
+                            obj.getJSONObject(i).getString("strMeasure8"),
+                            obj.getJSONObject(i).getString("strMeasure9"),
+                            obj.getJSONObject(i).getString("strMeasure10"),
+                            obj.getJSONObject(i).getString("strMeasure11"),
+                            obj.getJSONObject(i).getString("strMeasure12"),
+                            obj.getJSONObject(i).getString("strMeasure13"),
+                            obj.getJSONObject(i).getString("strMeasure14"),
+                            obj.getJSONObject(i).getString("strMeasure15"),
+                            obj.getJSONObject(i).getString("strImageSource"),
+                            obj.getJSONObject(i).getString("strImageAttribution"),
+                            obj.getJSONObject(i).getString("strCreativeCommonsConfirmed"),
+                            obj.getJSONObject(i).getString("dateModified")));
+                }
+                liveData.setValue(model);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
