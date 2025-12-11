9ee59efd97fe5e6ed52431504c5bb30b2ae21ee9
Patrick <binary_thinker@yahoo.com>
2025-12-10 19:38:27 -0800

    RecyclerView implementation. Code is 90% complete.

---
+++ app/src/main/java/edu/sfsu/thecocktaildb/ViewModel/SingletonViewModel.java
@@ -0,0 +1,29 @@
+package edu.sfsu.thecocktaildb.ViewModel;
+
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+
+import edu.sfsu.thecocktaildb.Model.CocktailModel;
+
+public class SingletonViewModel {
+    private static final SingletonViewModel obj = new SingletonViewModel();
+
+    private final ArrayList<CocktailModel> model;
+
+    public static SingletonViewModel getInstance() {
+        return obj;
+    }
+
+    private SingletonViewModel() {
+        model = new ArrayList<>();
+    }
+
+    public ArrayList<CocktailModel> getData() {
+        return model;
+    }
+
+    public MutableLiveData<CocktailModel> newMutableLiveData() {
+        return new MutableLiveData<>();
+    }
+}
